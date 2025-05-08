5a91a2ccf0b696744e7507f2bb8e7e77361bf4b1
Mikolaj-Wujek <mikolajwork1@gmail.com>
2025-05-08 19:55:04 +0100

    Added Weather API

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, session, redirect, url_for, g, request
 from database import get_db, close_db
 from flask_session import Session
 from functools import wraps
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENWEATHER_API_KEY")
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def index():
     
-    weather = {
-        'description': 'sunny',
-        'temp': '15',
-        'location': 'Cork'
-    }
+    weather = getWeather()
 
     clock = {
         'time': '15:00'
@@ -24,3 +26,21 @@ def index():
     }
 
     return render_template('index.html',weather=weather,clock=clock,spotify=spotify)
+
+def getWeather(location='Cork'):
+    api_key = os.getenv('OPENWEATHER_API_KEY')
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if request.status_code == 200:
+        data = response.json()
+        return {
+        'location': data['name'],
+        'temp': round(data['main']['temp']),
+        'description': data['weather'][0]['description'].capitalize()
+    }
+    else:  
+        return {
+            'location': location,
+            'temp': 'N/A',
+            'description': 'Error fetching weather'
+        }
